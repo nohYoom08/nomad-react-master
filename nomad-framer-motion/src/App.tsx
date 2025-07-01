6ef1c5fa293d7559a667853ff2a8d1ead0e409ec
coderOrlim <wnsgh1873@gmail.com>
2025-07-01 18:12:35 +0900

    🔥progress: 8.2

--- nomad-framer-motion/src/App.tsx
+++ nomad-framer-motion/src/App.tsx
@@ -10,9 +10,22 @@ const Wrapper = ({ children }: { children?: React.ReactNode }) => {
 
 const Box = ({ children }: { children?: React.ReactNode }) => {
     return (
-        <div className="w-[200px] h-[200px] bg-white rounded-[10px] shadow-[0_2px_3px_rgba(0,0,0,0.1),0_10px_20px_rgba(0,0,0,0.06)]">
+        //motion관련 대표적인 props 모음
+        <motion.div
+            className="w-[200px] h-[200px] bg-white rounded-[10px] shadow-[0_2px_3px_rgba(0,0,0,0.1),0_10px_20px_rgba(0,0,0,0.06)]"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1, borderRadius: '10%', rotateZ: 360 }}
+            transition={{
+                type: 'spring',
+                duration: 5,
+                ease: 'easeInOut',
+                damping: 3, // 진동감 조절 (type: 'spring'일 때 적용되는 속성. 기타 다른 속성에 대한 정보는 공식문서의 'spring' 섹션 참고)
+                bounce: 0.25, // 튕김 효과 조절
+                // stiffness: 1, // 강도 조절
+            }}
+        >
             {children}
-        </div>
+        </motion.div>
     );
 };
 
