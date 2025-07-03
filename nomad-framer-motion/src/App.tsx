4440d2c5536f9cbe9d4007d05ef255f66f999bad
coderOrlim <wnsgh1873@gmail.com>
2025-07-03 18:14:12 +0900

    🔥progress: 8.3

--- nomad-framer-motion/src/App.tsx
+++ nomad-framer-motion/src/App.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'motion/react';
+import { easeInOut, motion } from 'motion/react';
 
 const Wrapper = ({ children }: { children?: React.ReactNode }) => {
     return (
@@ -9,21 +9,28 @@ const Wrapper = ({ children }: { children?: React.ReactNode }) => {
 };
 
 const Box = ({ children }: { children?: React.ReactNode }) => {
-    return (
-        //motion관련 대표적인 props 모음
-        <motion.div
-            className="w-[200px] h-[200px] bg-white rounded-[10px] shadow-[0_2px_3px_rgba(0,0,0,0.1),0_10px_20px_rgba(0,0,0,0.06)]"
-            initial={{ scale: 0 }}
-            animate={{ scale: 1, borderRadius: '10%', rotateZ: 360 }}
-            transition={{
-                type: 'spring',
+    const myVars = {
+        start: { scale: 0 },
+        end: {
+            scale: 1,
+            borderRadius: '10%',
+            rotateZ: 360,
+            transition: {
+                type: 'spring' as const,
                 duration: 5,
-                ease: 'easeInOut',
+                ease: easeInOut, // easeInOut 함수 사용
+                // ease : 'easeInOut', // 이렇게 문자열로 하면 안 됨
+                // ease: [0.42, 0, 0.58, 1] as [number, number, number, number], // 또는 이렇게 배열로도 가능
                 damping: 3, // 진동감 조절 (type: 'spring'일 때 적용되는 속성. 기타 다른 속성에 대한 정보는 공식문서의 'spring' 섹션 참고)
                 bounce: 0.25, // 튕김 효과 조절
                 // stiffness: 1, // 강도 조절
-            }}
-        >
+            },
+        },
+    };
+
+    return (
+        <motion.div variants={myVars} initial="start" animate="end">
+            {/* 이 때 start, end등의 이름은 variants내의 프로퍼티로 들어가게 됨 */}
             {children}
         </motion.div>
     );
