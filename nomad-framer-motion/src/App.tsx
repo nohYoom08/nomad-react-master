9d0415f28e1b8626791f9858e02188057ce5a450
coderOrlim <wnsgh1873@gmail.com>
2025-07-07 18:06:30 +0900

    🔥progress: 8.9 - MotionValues part Three

--- nomad-framer-motion/src/App.tsx
+++ nomad-framer-motion/src/App.tsx
@@ -2,51 +2,45 @@ import {
     motion,
     MotionValue,
     useMotionValue,
+    useScroll,
     useTransform,
 } from 'motion/react';
 import { forwardRef, useEffect, useRef } from 'react';
 
-const Wrapper = ({ children }: { children?: React.ReactNode }) => {
+const Wrapper = forwardRef<
+    HTMLDivElement,
+    { children?: React.ReactNode; gradient: MotionValue<string> }
+>(({ children, gradient }, ref) => {
     return (
-        <div className="h-full w-full flex justify-center items-center bg-blue-500">
+        <motion.div
+            ref={ref}
+            className="h-[200dvh] w-full flex justify-center items-center"
+            style={{
+                background: gradient,
+            }}
+        >
             {children}
-        </div>
+        </motion.div>
     );
-};
-
-//forwardRef: 부모컴포넌트에서 BiggerBox에 ref속성을 직접 작성할 수 있도록 하는 문법
-const BiggerBox = forwardRef<HTMLDivElement, { children?: React.ReactNode }>(
-    ({ children }, ref) => {
-        return (
-            <div
-                ref={ref}
-                className="h-[600px] w-[600px] flex justify-center items-center rounded-[40px] bg-[rgba(255,255,255,0.6)]"
-            >
-                {children}
-            </div>
-        );
-    },
-);
+});
 
 const Box = ({
     children,
-    biggerBoxRef,
+    wrapperRef,
     x,
     potato, // useTransform을 통해 변환된 값
+    scale, // 스크롤 위치를 나타내는 MotionValue
 }: {
     children?: React.ReactNode;
-    biggerBoxRef: React.RefObject<HTMLDivElement | null>;
-    // biggerBoxRef: React.RefObject<HTMLDivElement> | null; 이라고 작성하면 RefObject에 대한 타입이 아닌 그냥 썡 null이 들어가게 됨
-    // 밑에 dragConstraints={biggerBoxRef}에서 biggerBoxRef가 null이 될 수 없기 때문에 오류가 발생함
+    wrapperRef: React.RefObject<HTMLDivElement | null>;
     x: MotionValue<number>;
     //MotionValue의 타입
     potato: MotionValue<number>;
+    scale: MotionValue<number>;
 }) => {
     const boxVariants = {
         start: {},
         end: {},
-        hover: { scale: 1.5, rotateZ: 360 },
-        click: { scale: 1, borderRadius: '100%' },
         drag: {
             backgroundColor: 'rgba(255, 150, 0, 1)',
             transition: { duration: 0.2 },
@@ -55,16 +49,14 @@ const Box = ({
 
     return (
         <motion.div
-            style={{ x, scale: potato }}
+            style={{ x, rotateZ: potato, scale }}
             className="w-[200px] h-[200px] bg-white rounded-[40px] shadow-[0_2px_3px_rgba(0,0,0,0.1),0_10px_20px_rgba(0,0,0,0.06)]"
             variants={boxVariants}
             initial="start"
             animate="end"
             whileHover="hover" // hover는 마우스가 요소 위에 있을 때 적용되는 애니메이션 (variants 내에 정의된 hover 상태를 사용)
-            whileTap="click" // click은 요소가 클릭되었을 때 적용되는 애니메이션 (variants 내에 정의된 click 상태를 사용)
             drag="x"
-            // dragConstraints={{ left: -200, right: 200, top: -200, bottom: 200 }}
-            dragConstraints={biggerBoxRef} // 부모컴포넌트 내부에서만 드래그 가능
+            dragConstraints={wrapperRef}
             dragSnapToOrigin // 드래그가 끝나면 원래 위치로 돌아감
             dragElastic={0}
             // 드래그 제한 범위 바깥에서 얼마나 탄력적으로 움직일지 설정
@@ -78,19 +70,39 @@ const Box = ({
 };
 
 export default function App() {
-    const biggerBoxRef = useRef<HTMLDivElement>(null);
+    const wrapperRef = useRef<HTMLDivElement>(null);
     const x = useMotionValue(0);
     //애니메이션 이동을 하는데 console.log는 안 뜸 -> 리렌더링 없이 애니메이션만 실행된다는 뜻임 -> ReactJS 생태계에 있는 것이 아닌 것 (엄청난!)
-    const potato = useTransform(x, [-800, 0, 800], [5, 1, 0.1]);
-    //useMotionValue() px의 단위를 변환시켜줌 (-800~800을 5~0.1로 변환)
-    useEffect(() => x.onChange(() => console.log(potato.get())), [x]);
+    const potato = useTransform(x, [-800, 800], [-360, 360]);
+    const gradient = useTransform(
+        x,
+        [-800, 0, 800],
+        [
+            'linear-gradient(135deg, rgb(0, 238, 198), rgb(0, 146, 85))',
+            'linear-gradient(135deg, rgb(238,0,154), rgb(244, 145, 251))',
+            'linear-gradient(135deg, rgb(234, 238, 0), rgb(241, 124, 0))',
+        ],
+    );
+    //useMotionValue() px의 단위를 변환시켜줌2 => 문자열로도 변환이 가능 -> 색깔변화에도 활용 가능, 다만 이건 tailwindcss로 불가능
+    const { scrollY, scrollYProgress } = useScroll();
+    //useScroll() 스크롤 위치를 가져오는 훅, scrollY는 px단위, scrollYProgress는 0~1사이의 값으로 스크롤 위치를 나타냄
+    const scale = useTransform(scrollYProgress, [0, 1], [0, 3]);
+    useEffect(() => {
+        scrollY.on('change', () =>
+            console.log(scrollY.get(), scrollYProgress.get()),
+        );
+    }, [scrollYProgress, scrollY]);
+
     return (
-        <Wrapper>
+        <Wrapper ref={wrapperRef} gradient={gradient}>
             <button onClick={() => x.set(200)}>click me</button>
-            <BiggerBox ref={biggerBoxRef}>
-                <Box biggerBoxRef={biggerBoxRef} x={x} potato={potato}></Box>
-                {/* Box태그에 style={{x}}를 작성하여 useMotionValue를 연결할 예정 */}
-            </BiggerBox>
+            <Box
+                x={x}
+                wrapperRef={wrapperRef}
+                potato={potato}
+                scale={scale}
+            ></Box>
+            {/* Box태그에 style={{x}}를 작성하여 useMotionValue를 연결할 예정 */}
         </Wrapper>
     );
 }
