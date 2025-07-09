dc0d6cecf10723b0904b89955d0a367250cd704a
coderOrlim <wnsgh1873@gmail.com>
2025-07-09 16:50:06 +0900

    🔥progress: 8.11 - AnimatePresence

--- nomad-framer-motion/src/App.tsx
+++ nomad-framer-motion/src/App.tsx
@@ -1,12 +1,12 @@
-import { motion } from 'motion/react';
-import { forwardRef, useRef } from 'react';
+import { AnimatePresence, motion } from 'motion/react';
+import { forwardRef, useRef, useState } from 'react';
 
 const Wrapper = forwardRef<HTMLDivElement, { children?: React.ReactNode }>(
     ({ children }, ref) => {
         return (
             <motion.div
                 ref={ref}
-                className="h-full w-full flex justify-center items-center bg-blue-300"
+                className="h-full w-full flex flex-col justify-center items-center gap-[40px] bg-blue-300"
             >
                 {children}
             </motion.div>
@@ -14,39 +14,50 @@ const Wrapper = forwardRef<HTMLDivElement, { children?: React.ReactNode }>(
     },
 );
 
-export default function App() {
-    const wrapperRef = useRef<HTMLDivElement>(null);
-    const svgVariants = {
-        start: {
-            pathLength: 0,
-            fill: 'rgba(255,255,255,0)',
+const Box = ({ children }: { children?: React.ReactNode }) => {
+    const boxVariants = {
+        initial: {
+            opacity: 0,
+            scale: 0,
         },
-        end: {
-            pathLength: 1,
-            fill: 'rgba(255,255,255,1)',
-            // transition: { duration: 2 }, //transition은 end에 들어감
+        visible: {
+            opacity: 1,
+            scale: 1,
+            rotateZ: 360,
+        },
+        leaving: {
+            opacity: 0,
+            y: 20,
         },
     };
+
+    return (
+        <motion.div
+            className="w-[200px] h-[200px] bg-white rounded-[40px] shadow-[0_2px_3px_rgba(0,0,0,0.1),0_10px_20px_rgba(0,0,0,0.06)]"
+            variants={boxVariants}
+            initial="initial"
+            animate="visible"
+            exit="leaving" // exit는 사라질 때 사용되는 애니메이션 (AnimatePresence 사용 시 활용)
+        >
+            {/* 이 때 start, end등의 이름은 variants내의 프로퍼티로 들어가게 됨 */}
+            {children}
+        </motion.div>
+    );
+};
+
+export default function App() {
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const [showing, setShowing] = useState(false);
+    const onClickShowing = () => setShowing(prev => !prev);
     return (
         <Wrapper ref={wrapperRef}>
-            <svg
-                className="w-[300px] h-[300px]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
+            <button
+                className="absolute top-20 left-[50%] transform -translate-x-1/2"
+                onClick={onClickShowing}
             >
-                <motion.path
-                    variants={svgVariants}
-                    initial="start"
-                    animate="end"
-                    transition={{
-                        default: { duration: 6 }, //모든 속성에 대한 기본 지속 시간
-                        fill: { duration: 2, delay: 2 }, //특정 속성에 대한 지속시간을 이렇게 따로 설정 가능 (varaints가 아닌, 여기서 직접 설정해주어야 함)
-                    }}
-                    stroke="white" //svg의 외곽선 설정
-                    strokeWidth="5"
-                    d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z"
-                />
-            </svg>
+                Click
+            </button>
+            <AnimatePresence> {showing ? <Box></Box> : null}</AnimatePresence>
         </Wrapper>
     );
 }
